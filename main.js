f1877d887ef317b1df5755747ecab1c9bfc7fa55
Vrishketu Ray <vrishketuray0@gmail.com>
2025-12-27 17:30:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // ================== API KEYS ==================
-const geminiAPIKey = "AIzaSyDUXZLjeQwcBvlgGnVINbqreB2XrYiqWn8";
+const geminiAPIKey = "AIzaSyBRROyhUOqa5RcJDAZ7zI3NmYDvmmy6kyM";
 // === Google Search API Keys ===
 const GOOGLE_API_KEY = "AIzaSyDf6fjW4nMrdM3V1fvM8sO2kS8gbvrEKBQ";
 const SEARCH_ENGINE_ID = "66e5361e4c83040a8";
